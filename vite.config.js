4274f60044a8ad9f5d29469096cf8d3d862d9843
Marcelo <m_cerutti72@hotmail.com>
2025-04-24 11:42:02 -0300

    sigo configurando

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  https:'//marceceru.github.io/social-link/'
+  base: 'https://marceceru.github.io/social-link/',
+
 })
